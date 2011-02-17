aa9bd6f9524c759cd51b7354b9cb79909da54923
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-02-17 21:24:04 +0100

    make_release.sh: more fixes

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -27,10 +27,10 @@ shopt -s extglob
 [ -z "${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))}" ] || die "release has the wrong form (X.X_rcX)"
 
 set -e
-[ -d "builddir" ] && die "builddir is already there"
+[ -d "builddir" ] && die "builddir is already there, run 'rm -rf $PWD/builddir'"
 mkdir builddir
 #order matters for deps
-#and pristine before not pristine
+#and pristine before not pristine to overwrite less files by more files
 for p in tools_pristine tools csg; do
 	[ -z "${p%%*_pristine}" ] && dist="dist-pristine" || dist="dist"
 	prog="${p%_pristine}"
@@ -43,7 +43,7 @@ for p in tools_pristine tools csg; do
 	#maybe version has not changed
 	hg commit -m "Version bumped to $rel" configure.ac || true
 	cd ..
-	./buildutil/build.sh --no-wait --no-rpath --prefix $PWD/build --$dist --clean-ignored --clean-out $prog || die
+	./buildutil/build.sh --no-wait --no-rpath --prefix $PWD/build --$dist --clean-ignored $prog || die
 	hg -R $prog tag -f "release_$rel"
 done	
 rm -rf builddir
@@ -51,9 +51,10 @@ cd buildutil
 sed -i "s/^\(latest\)=\".*\"$/\1=\"$rel\"/" build.sh || die "sed of build.sh failed"
 hg commit -m "bumped latest to $rel" build.sh || true
 
-echo "####### TODO by you #########
+echo "####### TODO by you #########"
 echo cd $PWD
 for p in tools csg buildutil; do
 	echo hg push -R $p
 done
+echo "upload_tarball" *$rel*
 
