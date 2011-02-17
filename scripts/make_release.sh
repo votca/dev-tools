77d007205a38595792ad3ad19840b0e5e1b79da8
Christoph Junghans <christoph.junghans@googlemail.com>
2011-02-17 20:54:36 +0100

    make_release.sh: minor fixes

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -7,10 +7,10 @@ die () {
   exit 1
 }
 
-[ -z "$2" ] && die "${0##*/}: missing argument add the path where to build the release and the release to make"
+[ -z "$2" ] && die "${0##*/}: missing argument.\nUsage ${0##*/} rel_version buildir"
 
-[ -d "$1" ] || die "Argument is not a dir"
-cd "$1"
+[ -d "$2" ] || die "Argument is not a dir"
+cd "$2"
 
 if [ -d buildutil ]; then
   cd buildutil
@@ -22,21 +22,19 @@ else
   hg clone $burl buildutil 
 fi
 
-rel="$2"
-
+rel="$1"
 shopt -s extglob
-[ -z "${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))}" ] || die "release has the wrong form"
+[ -z "${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))}" ] || die "release has the wrong form (X.X_rcX)"
 
 set -e
 [ -d "builddir" ] && die "builddir is already there"
 mkdir builddir
-cd builddir
 #order matters for deps
-#and pristine after not pristine
-for p in tools tools_pristine csg; do
+#and pristine before not pristine
+for p in tools_pristine tools csg; do
 	[ -z "${p%%*_pristine}" ] && dist="dist-pristine" || dist="dist"
 	prog="${p%_pristine}"
-	../buildutil/build.sh --no-wait --just-update $prog || die "build -U failed" #clone and checkout
+	./buildutil/build.sh --no-wait --just-update $prog || die "build -U failed" #clone and checkout
 	cd $prog
 	[ -z "$(hg status -mu)" ] || die "There are modified or unknown files in $p"
 	hg checkout stable || die "Could not checkout stable"
@@ -45,15 +43,15 @@ for p in tools tools_pristine csg; do
 	#maybe version has not changed
 	hg commit -m "Version bumped to $rel" configure.ac || true
 	cd ..
-	../buildutil/build.sh --no-wait --no-rpath --prefix $PWD/build --$dist --clean-ignored --clean-out $prog || die
+	./buildutil/build.sh --no-wait --no-rpath --prefix $PWD/build --$dist --clean-ignored --clean-out $prog || die
 	hg -R $prog tag -f "release_$rel"
 done	
-cd ..
 rm -rf builddir
 cd buildutil
-sed -i "s/^\(latest\)=\".*\"$/\1=\"$rel\"/" build.sh
+sed -i "s/^\(latest\)=\".*\"$/\1=\"$rel\"/" build.sh || die "sed of build.sh failed"
 hg commit -m "bumped latest to $rel" build.sh || true
 
+echo "####### TODO by you #########
 echo cd $PWD
 for p in tools csg buildutil; do
 	echo hg push -R $p
