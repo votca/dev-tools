badd139acfbf85e305a4a230269a6d3027dd6bd7
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-06-17 17:17:56 +0200

    make_release.sh - added buildtest from tarball and clean up (fixes issue 101)

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -27,14 +27,16 @@ fi
 
 rel="$1"
 shopt -s extglob
-[[ -z ${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))} ]] || die "release has the wrong form (X.X_rcX)"
+[[ $stable = "stable" && -n ${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))} ]] && die "release has the wrong form (X.X_rcX)"
 
 set -e
-instdir="$instdir"
+instdir="instdir"
+build="build"
+what="tools csg manual tutorials"
 [[ -d $instdir ]] && die "$instdir is already there, run 'rm -rf $PWD/$instdir'"
 #order matters for deps
 #and pristine before non-pristine to 'overwrite less components by more components'
-for p in tools_pristine tools csg manual; do
+for p in tools_pristine $what; do
 	[[ -z ${p%%*_pristine} ]] && dist="dist-pristine" || dist="dist"
 	prog="${p%_pristine}"
 	./buildutil/build.sh --no-wait --just-update $prog || die "build -U failed" #clone and checkout
@@ -45,28 +47,37 @@ for p in tools_pristine tools csg manual; do
 	if [[ $stable != "stable" ]]; then
           :
 	elif [[ $p = "manual" ]]; then
-	  :
-	#autotools to be removed soon
-	elif [[ -f configure.ac ]]; then
-	  sed -i "/AC_INIT/s/,[^,]*,\(bugs@votca.org\)/,$rel,\1/" configure.ac || die "sed of configure.ac failed"
+	  sed -i "/^VER=.*$/VER=$rel/" Makefile || die "sed of Makefile failed"
 	  #|| true because maybe version has not changed
-	  hg commit -m "Version bumped to $rel" configure.ac || true
+	  hg commit -m "Version bumped to $rel" Makefile || true
 	else
 	  sed -i "/set(PROJECT_VERSION/s/\"[^\"]*\"/\"$rel\"/" CMakeLists.txt || die "sed of CMakeLists.txt failed"
 	  #|| true because maybe version has not changed
 	  hg commit -m "Version bumped to $rel" CMakeLists.txt || true
 	fi
 	cd ..
-	if [[ $p = "manual" ]]; then
-	  ./buildutil/build.sh --no-wait --prefix $PWD/$instdir --clean-ignored $prog || die
-	  cp manual/manual.pdf votca-manual-$rel.pdf || die
-	else
-	  ./buildutil/build.sh --no-wait --prefix $PWD/$instdir --$dist --clean-ignored $prog || die
-	fi
+	./buildutil/build.sh --no-wait --prefix $PWD/$instdir --$dist --clean-ignored $prog || die
 	#we tag the release when the non-pristine version was build
 	[[ $stable = "stable" && -n ${p%%*_pristine} ]] && hg -R $prog tag -f "release_$rel"
 done	
 rm -rf $instdir
+mkdir $instdir
+[ -d $build ] && die "$build is already there, run 'rm -rf $PWD/$build'"
+mkdir $build
+cd $build
+for p in tools_pristine $what; do
+  prog="${p%_pristine}"
+  [[ $prog = "manual" ]] && continue
+  [[ -z ${p%%*_pristine} ]] && r="${rel}_pristine" || r="$rel"
+  [[ -z ${p%%*_pristine} ]] && o="-DEXTERNAL_BOOST=ON" || o="-DEXTERNAL_BOOST=OFF"
+  cp ../votca-$prog-$r.tar.gz . 
+  ../buildutil/build.sh --no-wait --prefix $PWD/../$instdir --no-relcheck --release $r $o --selfdownload $prog
+  rm -rf *
+done
+cd ..
+rm -rf $build
+rm -rf $instdir
+
 cd buildutil
 if [[ $stable = "stable" ]]; then
   sed -i "s/^\(latest\)=\".*\"$/\1=\"$rel\"/" build.sh || die "sed of build.sh failed"
@@ -87,9 +98,8 @@ cd ..
 if [[ $stable = "stable" ]]; then
   echo "####### TODO by you #########"
   echo cd $PWD
-  for p in tools csg buildutil manual; do
-  	echo hg push -R $p
-  done
+  echo "for p in $what; do hg out -R \$p; done"
+  echo "for p in $what; do hg push -R \$p; done"
   echo "uploads tarball" *$rel* *.pdf
 else
   echo cd $PWD
