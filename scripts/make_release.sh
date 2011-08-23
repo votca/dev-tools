53af0653083eca229cb2546409ba46b4de65cc04
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-08-23 15:45:20 +0200

    make_release.sh: added csgapps to the release and fixed a lot of small issues

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,5 +1,9 @@
 #! /bin/bash -e
 
+#TODO
+#- allow $what to be more flexible
+
+
 burl="https://votca.googlecode.com/hg/"
 stable=stable
 
@@ -8,11 +12,13 @@ die () {
   exit 1
 }
 
+unset CSGSHARE VOTCASHARE
+
 [[ $1 = "--test" ]] && stable="default" && shift
 
-[[ -z $2 ]] && die "${0##*/}: missing argument.\nUsage ${0##*/} [--test] rel_version buildir"
+[[ -z $2 ]] && die "${0##*/}: missing argument.\nUsage ${0##*/} [--test] rel_version builddir"
 
-[[ -d $2 ]] || die "Argument is not a dir"
+[[ -d $2 ]] || mkdir -p "$2"
 cd "$2"
 
 if [[ -d buildutil ]]; then
@@ -27,13 +33,13 @@ fi
 
 rel="$1"
 shopt -s extglob
-[[ $stable = "stable" && -n ${rel//[1-9].[0-9]?(_rc[1-9]?([0-9]))} ]] && die "release has the wrong form (X.X_rcX)"
+[[ $stable = "stable" && ${rel} = [1-9].[0-9]?(.[1-9]|_rc[1-9])?(_pristine) ]] || die "release has the wrong form"
 
 set -e
 instdir="instdir"
 build="build"
-what="tools csg manual tutorials"
-[[ -d $instdir ]] && die "$instdir is already there, run 'rm -rf $PWD/$instdir'"
+what="tools csg csgapps manual tutorials"
+[[ -d $instdir ]] && die "Test install dir '$instdir' is already there, run 'rm -rf $PWD/$instdir'"
 #order matters for deps
 #and pristine before non-pristine to 'overwrite less components by more components'
 for p in tools_pristine $what; do
@@ -47,18 +53,22 @@ for p in tools_pristine $what; do
 	if [[ $stable != "stable" ]]; then
           :
 	elif [[ $p = "manual" ]]; then
-	  sed -i "/^VER=.*$/VER=$rel/" Makefile || die "sed of Makefile failed"
-	  #|| true because maybe version has not changed
-	  hg commit -m "Version bumped to $rel" Makefile || true
-	else
+	  sed -i "s/^VER=.*$/VER=$rel/" Makefile || die "sed of Makefile failed"
+	elif [[ -f CMakeLists.txt ]]; then
 	  sed -i "/set(PROJECT_VERSION/s/\"[^\"]*\"/\"$rel\"/" CMakeLists.txt || die "sed of CMakeLists.txt failed"
+	fi
+	if [[ $stable = "stable" ]]; then
+	  #remove old tags
+	  if [[ -f .hgtags ]]; then
+	    sed -i "/release_${rel}$/d" .hgtags
+	  fi
 	  #|| true because maybe version has not changed
-	  hg commit -m "Version bumped to $rel" CMakeLists.txt || true
+	  hg commit -m "Version bumped to $rel" || true
 	fi
 	cd ..
 	REL="$rel" ./buildutil/build.sh --no-wait --prefix $PWD/$instdir --$dist --clean-ignored $prog || die
 	#we tag the release when the non-pristine version was build
-	[[ $stable = "stable" && -n ${p%%*_pristine} ]] && hg -R $prog tag -f "release_$rel"
+	[[ $stable = "stable" && -n ${p%%*_pristine} ]] && hg -R $prog tag "release_$rel"
 done	
 rm -rf $instdir
 mkdir $instdir
@@ -69,9 +79,9 @@ for p in tools_pristine $what; do
   prog="${p%_pristine}"
   [[ $prog = "manual" ]] && continue
   [[ -z ${p%%*_pristine} ]] && r="${rel}_pristine" || r="$rel"
-  [[ -z ${p%%*_pristine} ]] && o="-DEXTERNAL_BOOST=ON" || o="-DEXTERNAL_BOOST=OFF"
+  [[ -z ${p%%*_pristine} ]] && opts="-DEXTERNAL_BOOST=ON" || opts="-DEXTERNAL_BOOST=OFF"
   cp ../votca-$prog-$r.tar.gz . 
-  ../buildutil/build.sh --no-wait --prefix $PWD/../$instdir --no-relcheck --release $r $o --selfdownload $prog
+  ../buildutil/build.sh --no-wait --prefix $PWD/../$instdir --release $r $opts --selfdownload $prog
   rm -rf *
 done
 cd ..
