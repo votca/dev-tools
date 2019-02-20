2cbcfda23beb696da9ea32e3c6a96bf64e205767
Joshua S Brown <joshbro42867@yahoo.com>
2019-02-20 12:33:40 -0700

    Added extra line`
    Extra line based on Christoph's implementation.

--- docker_clean.sh
+++ docker_clean.sh
@@ -29,6 +29,8 @@
 # 0 0 * * 1 ~/docker_clean.sh > /dev/null 2>&1
 #
 # Save and close the crontab file. At this point, every midnight the Docker cleanup task will run and you'll always enjoy a fresh and clean Docker experience.
-
+docker ps -aqf "status=exited" --no-trunc | xargs docker rm
+# Remove dangling images
 docker rmi $(docker images -q -f dangling=true)
+# Remove dangling volumes
 docker volume rm $(docker volume ls -qf dangling=true)
